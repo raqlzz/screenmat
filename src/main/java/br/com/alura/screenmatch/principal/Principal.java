d5b90e7614a051f1406d9db468f1ae6f697cab4b
raqlzz <raqlzz03@gmail.com>
2024-06-05 06:56:52 -0300

    criando uma classe de negócios episódios e comecando uma busca com datas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,15 +3,20 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-    public class Principal {
+
+public class Principal {
 
         private Scanner leitura = new Scanner(System.in);
         private ConsumoApi consumo = new ConsumoApi();
@@ -20,23 +25,48 @@ import java.util.Scanner;
         private final String ENDERECO = "https://www.omdbapi.com/?t=";
         private final String API_KEY = "&apikey=6585022c";
 
-        public void exibeMenu() throws JsonProcessingException {
-            System.out.println("Digite o nome da série para a busca:");
-            var nomeSerie = leitura.nextLine();
-            var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-            DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-            System.out.println(dados);
-            List<DadosTemporada> temporadas = new ArrayList<>();
+            public void exibeMenu() throws JsonProcessingException {
+                System.out.println("Digite o nome da série para a busca:");
+                var nomeSerie = leitura.nextLine();
+                var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+                DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+                System.out.println(dados);
+                List<DadosTemporada> temporadas = new ArrayList<>();
 
-            for(int i = 1; i<=dados.totalTemporadas(); i++) {
-                json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
-                DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-                temporadas.add(dadosTemporada);
+                for(int i = 1; i<=dados.totalTemporadas(); i++) {
+                    json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+                    DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+                    temporadas.add(dadosTemporada);
 
-            }
-            temporadas.forEach(System.out::println);
+                }
+                temporadas.forEach(System.out::println);
 
-            temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+                temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+                List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                        .flatMap(t -> t.episodios().stream())
+                        .collect(Collectors.toList());
+
+            System.out.println("\nTop 5 apisodios");
+            dadosEpisodios.stream()
+                    .filter(e -> e.avaliacao().equalsIgnoreCase("N/A"))
+                    .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                    .limit(5)
+                    .forEach(System.out::println);
+
+
+            List<Episodio>episodios = temporadas.stream()
+                    .flatMap(t -> t.episodios().stream()
+                            .map(d-> new Episodio(t.numero(), d))
+                            ).collect(Collectors.toList());
+
+            episodios.forEach(System.out::println);
+
+                System.out.println("A partir de que ano voce deseja ver os episódios?");
+                var ano = leitura.nextInt();
+                leitura.nextLine();
+
+                LocalDate dataBusca = LocalDate.of(ano,1,1);
 
         }
 
